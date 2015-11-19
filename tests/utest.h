08e3e69ce46842a5b77292d1099fea3ba6e049fa
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-19 13:02:58 +0100

    affichage correct des messages dans les tests

--- tests/utest.h
+++ tests/utest.h
@@ -9,12 +9,13 @@
 
 #include <stdio.h>
 
-#define FAIL(message) do { fprintf(stderr,"FAILURE: "##message##"\n"); return; } while(0)
-#define OK(message) do { fprintf(stderr,"SUCCESS: "##message##"\n"); } while(0)
+#define FAIL(message, test) do { fprintf(stderr,"FAILURE:(%s) %s; (%s, line %d)\n", #test, message, __FILE__, __LINE__); return; } while(0)
+#define OK(message) do { fprintf(stderr,"SUCCESS: %s\n", message); } while(0)
 
 
 #define u_assert(message, test) do { if (!(test)) FAIL(message); } while(0)
 #define u_isnull(message, test) do { if ((test) != NULL) FAIL(message); } while(0)
+#define u_isnotnull(message, test) do { if ((test) == NULL) FAIL(message); } while(0)
 #define u_success(message) do { OK(message); } while(0)
 
 #endif
