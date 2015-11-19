8ee640bf24e33b0002fd76afcb6f8dd383e482b3
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-19 18:11:31 +0100

    test2

---
+++ tests/test2.c
@@ -0,0 +1,74 @@
+#include "utest.h"
+#include "elempool.h"
+
+
+void allocate1k(struct Elem **head) {
+	for(int i=0; i < 1000; i++) {
+		struct Elem *e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+		
+		e->val = i;
+		e->next = *head;
+		*head = e;
+	}
+}
+
+
+void test2(void)
+{
+	/* Allocate all the 1000 elements */
+	struct Elem *head = NULL;
+	allocate1k(& head);
+	
+	/* No element should be free */
+	struct Elem *e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* check the list */
+	e = head;
+	for(int j = 0; j < 1000; j++) {
+		u_assert("incoherent val field in Elem",
+			 e->val == (1000 -j - 1));
+		e = e->next;
+	}
+
+	/* No element should be freed as all are in the list */
+	gcElems(& head, 1);
+
+	/* No element should be freed */
+	e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* check the list */
+	e = head;
+	for(int j = 0; j < 1000; j++) {
+		u_assert("incoherent val field in Elem",
+			 e->val == (1000 -j - 1));
+		e = e->next;
+	}
+	
+	/* free all elements */
+	head = NULL;
+	gcElems(& head, 1);
+		
+		
+	/* Allocate all the 1000 elements */
+	struct Elem *head = NULL;
+	for(int i=0; i < 1000; i++) {
+		struct Elem *e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+		
+		e->val = i;
+		e->next = head;
+		head = e;
+	}
+	/* No element should be free */
+	struct Elem *e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* free all elements */
+	head = NULL;
+	gcElems(& head, 1);
+
+	u_success("test2");
+}
