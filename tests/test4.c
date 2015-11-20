fbbba72d158b7a5da2700205491526e4ce9f412d
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-20 17:12:28 +0100

    test4: many short list of elements
    create 1000 list of one element. Free half. allocate 500 list of one element

--- tests/test4.c
+++ tests/test4.c
@@ -4,10 +4,65 @@
 
 void test4(void)
 {  
+	/* Allocate all the 1000 elements */
+	struct Elem *heads[1000] = {};
 	struct Elem *e = allocElem();
 	u_isnotnull("unexpected failure in allocation", e);
-	e->val = 10;
-	e->next = 0;
-	gcElems(& e, 1);
+
+	for(int i=0; i < 1000; i++) {
+		e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+		
+		e->val = i;
+		e->next = heads[i];
+		heads[i] = e;
+	}
+	/* No element should be free */
+	e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* check the list */
+	for(int i = 0; i < 1000; i++) {
+		u_assert("incoherent val field in Elem",
+			 heads[i]->val == i);
+		u_isnull("incoherent next field in Elem",
+			 heads[i]->next);
+	}
+
+	/* free none of the elements */
+	gcElems(heads, 1000);
+
+	/* No element should be free */
+	e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* free half of the elements */
+	gcElems(heads, 500);
+
+	/* allocate 500 elements */
+		for(int i=0; i < 1000; i++) {
+		e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+		
+		e->val = i;
+		e->next = heads[i];
+		heads[i] = e;
+	}
+	/* No element should be free */
+	e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* check the list */
+	for(int i = 0; i < 1000; i++) {
+		u_assert("incoherent val field in Elem",
+			 heads[i]->val == i);
+		u_isnull("incoherent next field in Elem",
+			 heads[i]->next);
+	}
+	
+	/* free all elements */
+	heads[0] = 0;
+	gcElems(heads, 0);
+	
 	u_success("test4");
 }
