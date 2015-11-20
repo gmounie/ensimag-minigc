83174a287f33eb543cb229a29a493197fa845f5b
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-20 13:49:45 +0100

    test3 with half of gc
    allocate 1000, gc 500, allocate 500, check the list

--- tests/test3.c
+++ tests/test3.c
@@ -3,11 +3,54 @@
 
 
 void test3(void)
-{  
+{
+	struct Elem *head = NULL;
 	struct Elem *e = allocElem();
-	u_isnotnull("unexpected failure in allocation", e);
-	e->val = 10;
-	e->next = 0;
-	gcElems(& e, 1);
+
+	/* allocate 2 * 500 but chain only one other 2 */
+	for(int i=0; i < 500; i++) {
+		e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+		
+		e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+		
+		e->val = i;
+		e->next = head;
+		head = e;
+	}
+	/* No element should be free */
+	e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* free the 500 elems not in the liste */
+	gcElems( & head, 1);
+
+	/* allocate 500 and chain them */	
+	for(int i=0; i < 500; i++) {
+		e = allocElem();
+		u_isnotnull("unexpected allocation failure", e);
+
+		e->val = 500 + i;
+		e->next = head;
+		head = e;
+	}
+	/* No element should be free */
+	e = allocElem();
+	u_isnull("unexpected allocation success", e);
+
+	/* check the list */
+	e = head;
+	for(int j = 0; j < 1000; j++) {
+		u_assert("incoherent val field in Elem",
+			 e->val == (1000 -j - 1));
+		e = e->next;
+	}
+	u_isnull("incoherent end of list", e);
+
+	/* free all elements */
+	head = NULL;
+	gcElems(& head, 1);
+
 	u_success("test3");
 }
