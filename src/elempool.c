610bb9b4e63ecab38fb89d7c8818f22dfe9d7e1a
Grégory Mounié <Gregory.Mounie@imag.fr>
2015-11-21 15:35:07 +0100

    solution de référence

--- src/elempool.c
+++ src/elempool.c
@@ -20,6 +20,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "elempool.h"
 #include "bitset1000.h"
 
@@ -30,6 +31,12 @@ static unsigned char *memoireElemPool = 0;
 */
 struct Elem* allocElem() {
 	/* ajouter votre code ici / add your code here */
+	for(int i = 0; i < 1000; i++) {
+		if (! bt1k_get(i)) {
+			bt1k_set(i, true);
+			return ((struct Elem*) memoireElemPool)+i;
+		}
+	}
 	return 0;
 }
 
@@ -39,11 +46,28 @@ struct Elem* allocElem() {
 */
 void gcElems( struct Elem ** heads, int nbheads) {
 	/* ajouter votre code ici / add your code here */
+	bt1k_reset();
+	for(int i=0; i < nbheads; i++) {
+		struct Elem *cour= heads[i];
+		while(cour != NULL) {
+			int index = (cour - ((struct Elem *) memoireElemPool));
+			bt1k_set(index, true);
+			cour = cour->next;
+		}
+	}
+}
+
+
+void freepool(void) {
+	assert(memoireElemPool != 0);
+	free(memoireElemPool);
 }
 
 void initElems() {
 	bt1k_reset();
-	if (! memoireElemPool)
-	  memoireElemPool = malloc(1000 * sizeof(struct Elem));
+	if (! memoireElemPool) {
+		memoireElemPool = malloc(1000 * sizeof(struct Elem));
+		atexit(freepool);
+	}
 	memset(memoireElemPool, 0, 1000 * sizeof(struct Elem));
 }
